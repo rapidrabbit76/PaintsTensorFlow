537e395a8badc8bccaa85106676e258499d2ca36
yslee <yslee.dev@gmail.com>
2022-03-09 20:44:15 +0900

    feat: generator model

---
+++ models/generator/blocks.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+from tensorflow import Tensor
+from tensorflow import keras
+from tensorflow.python.keras import Model, Sequential
+from tensorflow.python.keras.layers import (
+    Conv2D,
+    Conv2DTranspose,
+    LeakyReLU,
+)
+from tensorflow_addons.layers import InstanceNormalization
+
+
+__INITIALIZER__ = None
+Normalization = InstanceNormalization
+Activation = LeakyReLU
+
+
+class ConvBlock(Model):
+    def __init__(
+        self,
+        outp: int,
+        k: int,
+        s: int,
+        norm: bool = True,
+        act: bool = True,
+    ) -> None:
+        super().__init__()
+        layer = [
+            Conv2D(
+                outp,
+                k,
+                s,
+                padding="same",
+                kernel_initializer=__INITIALIZER__,
+                use_bias=False,
+            )
+        ]
+        if norm:
+            layer += [Normalization()]
+        if act:
+            layer += [Activation(0.2)]
+        self.block = Sequential(layer)
+
+    def call(self, x: Tensor, training: bool = None) -> Tensor:
+        return self.block(x, training=training)
+
+
+class ConvUpBlock(Model):
+    def __init__(
+        self,
+        outp: int,
+        k: int,
+        s: int,
+        norm: bool = True,
+        act: bool = True,
+    ) -> None:
+        super().__init__()
+        layer = [
+            Conv2DTranspose(
+                outp,
+                k,
+                s,
+                padding="same",
+                kernel_initializer=__INITIALIZER__,
+                use_bias=False,
+            )
+        ]
+        if norm:
+            layer += [Normalization()]
+        if act:
+            layer += [Activation(0.2)]
+        self.block = Sequential(layer)
+
+    def call(self, x: Tensor, training: bool = None) -> Tensor:
+        return self.block(x, training=training)
