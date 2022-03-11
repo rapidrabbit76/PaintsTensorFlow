8538d0bc38f4fddaa487f4f17533cefd82bc1ab8
yslee <yslee.dev@gmail.com>
2022-03-11 13:38:33 +0900

    feat: colorization transforms

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -69,3 +69,57 @@ class DraftTransforms:
         hint = tf.identity(color)
 
         return line, hint, color
+
+
+class ColorizationTransforms(DraftTransforms):
+    def __init__(
+        self,
+        image_size: int,
+        draft_image_r: float,
+        train: Mode,
+    ):
+        draft_image = image_size // draft_image_r
+        self.both_transforms = A.Compose(
+            [
+                A.Resize(image_size, image_size, interpolation=cv2.INTER_AREA),
+                A.RandomResizedCrop(image_size, image_size, p=1)
+                if train is Mode.TRAIN
+                else A.Resize(draft_image, draft_image),
+                A.HorizontalFlip(p=0.5 if train is Mode.TRAIN else 0),
+            ],
+            additional_targets={"image0": "image"},
+        )
+        self.draft_transforms = A.Compose(
+            A.Resize(draft_image, draft_image, interpolation=cv2.INTER_AREA),
+            additional_targets={"image0": "image"},
+        )
+
+    def b_transforms(self, line, color):
+        super().b_transforms(line, color)
+        data = {
+            "image": line,
+            "image0": color,
+        }
+
+        augmentations = self.draft_transforms(**data)
+        line_draft = augmentations["image"]
+        hint = augmentations["image0"]
+        return [line, line_draft, hint, color]
+
+    def __call__(
+        self, line_path: str, color_path: str
+    ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
+        line = self.image_load(line_path, 1)
+        color = self.image_load(color_path, 3)
+
+        line, line_draft, hint, color = tf.numpy_function(
+            func=self.b_transforms,
+            inp=[line, color],
+            Tout=[tf.uint8, tf.uint8, tf.uint8, tf.uint8],
+        )
+        line = self.cast(line)
+        line_draft = self.cast(line)
+        hint = self.cast(hint)
+        color = self.cast(color)
+
+        return line, line_draft, hint, color
