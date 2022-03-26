b7c7341dd85634fda2e85a1c3af26f4e042b11e5
yslee <yslee.dev@gmail.com>
2022-03-26 13:27:44 +0900

    feat: add gen tensor spec

--- tests/test_model_inference.py
+++ tests/test_model_inference.py
@@ -1,12 +1,14 @@
 def test_draft_model(draft_model, draft_batch):
     line, hint, color = draft_batch
-    x = draft_model(line, hint)
+    input_dict = {"line": line, "hint": hint, "training": True}
+    x = draft_model(input_dict)
     assert list(x.shape) == list(color.shape)
 
 
 def test_disc_model(draft_model, disc, draft_batch):
     line, hint, color = draft_batch
-    x = draft_model(line, hint)
+    input_dict = {"line": line, "hint": hint, "training": True}
+    x = draft_model(input_dict)
     _logits = disc(x)
     logits = disc(color)
     assert list(_logits.shape) == list(logits.shape)
@@ -15,5 +17,6 @@ def test_disc_model(draft_model, disc, draft_batch):
 def test_colorization_model(colorization_model, colorization_batch):
     line, line_draft, hint, color = colorization_batch
     draft = color
-    _color = colorization_model(line, draft)
+    input_dict = {"line": line, "hint": draft, "training": True}
+    _color = colorization_model(input_dict)
     assert list(_color.shape) == list(color.shape)
